5798dd41ab9e365efa4f77543991686fdde648d6
Esteban <esteban.reyes@cimat.mx>
2020-10-09 22:30:22 -0500

    Agrego Funciones
    swap
    allocate
    free
    removemax
    topdownheapify

--- esteban-andrea/heap.c
+++ esteban-andrea/heap.c
@@ -10,6 +10,7 @@ typedef struct _THeap{
 }THeap;
 
 THeap * THeap_new(void);
+void    swap(int *arr, int i, int j){
 void    free_THeap(THeap ** hptr);
 
 void    insert(THeap *h, int data);
@@ -20,23 +21,16 @@ void    bottomUpHeapify(int *arr, int k);
 void    topDownHeapify(int * arr, int k, int n);
 
 // Definiciones ===============================================================
-THeap * THeap_new(void){
-  // Alocacion de memoria
-  THeap *heap = (THeap *) calloc (1,sizeof(THeap));
-  return heap;
-}
-void free_THeap(THeap ** hptr){
-  free((*hptr)->data);
-  free(*hptr);
-}
-
 
 void insert(THeap *h, int data){ 
   return;
 } 
 
 int removeMax(THeap *h){ 
-  return -1;
+  if (h->size < 1) return -1;
+  int tmp = h->data[1];
+  h->data[1] = h->data[h->size--];
+  topDownHeapify(h->data, 1, h->size);
 } 
 
 int getMax(THeap *h){ 
@@ -48,7 +42,31 @@ void bottomUpHeapify(int *arr, int k){
 } 
 
 void topDownHeapify(int * arr, int k, int n){ 
+  while (3*k <= n){
+    int j = 3*k;
+    if (j < n && arr[j] < arr[j+1]) j++;
+    if (arr[k]>= arr[j]) break;
+    swap(arr, k, j);
+    k = j;
+  }
   return;
 } 
+
+THeap * THeap_new(void){
+  // Alocacion de memoria
+  THeap *heap = (THeap *) calloc (1,sizeof(THeap));
+  return heap;
+}
+
+void swap(int *arr, int i, int j){
+  int tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
+void free_THeap(THeap ** hptr){
+  free((*hptr)->data);
+  free(*hptr);
+}
 
 #endif
